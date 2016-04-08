e0463dcbb94ead029e63bbb8bf704f44103504fb
Bernhard Berger <b.berger@contemas.net>
2016-04-08 15:35:37 +0200

    [CONFIG] Add PSR-4 Autoloader config

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,4 +32,39 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	)
+	'title' => 'Faceted Search',
+	'description' => 'Faceted fulltext search for TYPO3. Fast, flexible and easy to use. Very easy to install. Fast (tested with over 200.000 records) and flexible (you can write your own indexers). Indexes content directly from the databases. Visit kesearch.de for further information and documentation.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '2.2.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Christian Buelter (team.inmedias)',
+	'author_email' => 'christian.buelter@inmedias.de',
+	'author_company' => 'team.inmedias',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		    'php' => '5.3.0-0.0.0',
+		    'typo3' => '6.2.0-7.99.99',
+		),
+		'conflicts' => '',
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	)
+    'autoload' => array(
+        'psr-4' => array('TeaminmediasPluswerk\\KeSearch\\' => 'Classes'),
+    ),
 );
