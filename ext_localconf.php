47dcf4f098b9d0ace426be8b13abdaad459cbb20
Bernhard Berger <b.berger@contemas.net>
2016-04-11 19:00:43 +0200

    [TASK] Move pageTS to a more appropriate location

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,7 +10,9 @@ if (TYPO3_MODE=='BE')    {
 
 // include filterlist class and pageTSconfig.txt and add plugin
 include_once(TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . '/Classes/Backend/class.user_filterlist.php');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ke_search/pageTSconfig.txt">');
+
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ke_search/Configuration/TSConfig/pageTS.txt">');
+
 TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'pi1/class.tx_kesearch_pi1.php', '_pi1', 'list_type', 0);
 TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'pi2/class.tx_kesearch_pi2.php', '_pi2', 'list_type', 0);
 TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'pi3/class.tx_kesearch_pi3.php', '_pi3', 'list_type', 0);
