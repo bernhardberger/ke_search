22a9013f9dfa711acf983b8b08f0a0855f3812da
Bernhard Berger <b.berger@contemas.net>
2016-04-11 18:49:29 +0200

    [TASK] Clean up ext_tables.php

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,6 +1,21 @@
 <?php
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'ke_search',
     'Configuration/TypoScript',
     'Faceted Search'
 );
+
+// Show FlexForm field in plugin configuration
+$TCA['tt_content']['types']['list']['subtypes_addlist']['ke_search_pi1'] = 'pi_flexform';
+$TCA['tt_content']['types']['list']['subtypes_addlist']['ke_search_pi2'] = 'pi_flexform';
+$TCA['tt_content']['types']['list']['subtypes_addlist']['ke_search_pi3'] = 'pi_flexform';
+
+$TCA['tt_content']['types']['list']['subtypes_excludelist']['ke_search_pi1'] = 'layout,select_key';
+$TCA['tt_content']['types']['list']['subtypes_excludelist']['ke_search_pi2'] = 'layout,select_key';
+$TCA['tt_content']['types']['list']['subtypes_excludelist']['ke_search_pi3'] = 'layout,select_key';
+
+// Configure FlexForm field
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('ke_search_pi1', 'FILE:EXT:ke_search/pi1/flexform_pi1.xml');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('ke_search_pi2', 'FILE:EXT:ke_search/pi2/flexform_pi2.xml');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('ke_search_pi3', 'FILE:EXT:ke_search/pi3/flexform_pi3.xml');
--- ext_tables.php
+++ ext_tables.php
@@ -4,31 +4,12 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$extPath = TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('ke_search');
-$extRelPath = TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('ke_search');
-
-if (TYPO3_MODE == 'BE') {
-	require_once($extPath . 'Classes/lib/class.tx_kesearch_lib_items.php');
-}
-
-
-
 // help file
 TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_kesearch_filters', 'EXT:ke_search/locallang_csh.xml');
 
-// Show FlexForm field in plugin configuration
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi2'] = 'pi_flexform';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi3'] = 'pi_flexform';
-
-// Configure FlexForm field
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:ke_search/pi1/flexform_pi1.xml');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi2', 'FILE:EXT:ke_search/pi2/flexform_pi2.xml');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi3', 'FILE:EXT:ke_search/pi3/flexform_pi3.xml');
-
-
-
 if (TYPO3_MODE == 'BE') {
+	require_once(TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('ke_search') . 'Classes/lib/class.tx_kesearch_lib_items.php');
+
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
 		'TeaminmediasPluswerk.' . $_EXTKEY,
 		'web',          // Main area
@@ -43,41 +24,10 @@ if (TYPO3_MODE == 'BE') {
 			'labels'    => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_mod.xml',
 		)
 	);
-}
 
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key';
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi2'] = 'layout,select_key';
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi3'] = 'layout,select_key';
-
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
-	'LLL:EXT:ke_search/locallang_db.xml:tt_content.list_type_pi1',
-	$_EXTKEY . '_pi1',
-	$extRelPath . 'ext_icon.gif'), 'list_type'
-);
-
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
-	'LLL:EXT:ke_search/locallang_db.xml:tt_content.list_type_pi2',
-	$_EXTKEY . '_pi2',
-	$extRelPath . 'ext_icon.gif'), 'list_type'
-);
-
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
-	'LLL:EXT:ke_search/locallang_db.xml:tt_content.list_type_pi3',
-	$_EXTKEY . '_pi3',
-	$extRelPath . 'ext_icon.gif'), 'list_type'
-);
-
-// class for displaying the category tree for tt_news in BE forms.
-if (TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('tt_news')) {
-	include_once(TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('tt_news') . 'lib/class.tx_ttnews_TCAform_selectTree.php');
-}
-
-
-if (TYPO3_MODE == 'BE') {
-	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_kesearch_pi1_wizicon'] = $extPath . 'pi1/class.tx_kesearch_pi1_wizicon.php';
-	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_kesearch_pi2_wizicon'] = $extPath . 'pi2/class.tx_kesearch_pi2_wizicon.php';
-	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_kesearch_pi3_wizicon'] = $extPath . 'pi3/class.tx_kesearch_pi3_wizicon.php';
-
+	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_kesearch_pi1_wizicon'] = TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('ke_search') . 'pi1/class.tx_kesearch_pi1_wizicon.php';
+	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_kesearch_pi2_wizicon'] = TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('ke_search') . 'pi2/class.tx_kesearch_pi2_wizicon.php';
+	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_kesearch_pi3_wizicon'] = TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('ke_search') . 'pi3/class.tx_kesearch_pi3_wizicon.php';
 
 	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['file_parser_groups'] = array(
 		'pdf' => array(
@@ -126,4 +76,29 @@ if (TYPO3_MODE == 'BE') {
 			'extensions' =>  array('jpg', 'jpeg', 'tif')
 		)
 	);
+}
+
+
+
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
+	'LLL:EXT:ke_search/locallang_db.xml:tt_content.list_type_pi1',
+	$_EXTKEY . '_pi1',
+	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('ke_search') . 'ext_icon.gif'), 'list_type'
+);
+
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
+	'LLL:EXT:ke_search/locallang_db.xml:tt_content.list_type_pi2',
+	$_EXTKEY . '_pi2',
+	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('ke_search') . 'ext_icon.gif'), 'list_type'
+);
+
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
+	'LLL:EXT:ke_search/locallang_db.xml:tt_content.list_type_pi3',
+	$_EXTKEY . '_pi3',
+	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('ke_search') . 'ext_icon.gif'), 'list_type'
+);
+
+// class for displaying the category tree for tt_news in BE forms.
+if (TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('tt_news')) {
+	include_once(TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('tt_news') . 'lib/class.tx_ttnews_TCAform_selectTree.php');
 }
